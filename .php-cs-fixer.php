ae81a5bba89bb88f85c10a0c8f7d6ee3ce34044a
Mahdi Hazaveh <mahdi@hazaveh.net>
2023-10-13 17:26:17 +0200

    add php-cs-fixer and phpstan

---
+++ .php-cs-fixer.php
@@ -0,0 +1,14 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(['src', 'tests'])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+    ])
+    ->setFinder($finder)
+;
